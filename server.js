ae1ad13f99f77686e9595359e902ebc5e2c91656
MOULIMONISH S <moulimonish19@gmail.com>
2025-02-21 13:15:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS Configuration (Allow live frontend)
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://sample-brookbytes.netlify.app"], // Local + Live Frontend
+    origin: ["https://sample-brookbytes.netlify.app"], // Local + Live Frontend
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 };
